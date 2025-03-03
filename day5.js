0b9d8c73f2fa717d0fd38a67df5dc267bd345035
Rafael <rg.gallegostapia@gmail.com>
2025-03-03 19:21:23 -0300

    commit del dia 5 y 6

--- day5.js
+++ day5.js
@@ -1,7 +1,9 @@
+// Funcion que utiliza el array que se escogio(eleccion) e ingresa el nombre del producto en ella 
 function list(eleccion, producto) {
     eleccion.push(producto);
 }
 
+//Listas de categorias
 let frutas = [];
 let lacteos = [];
 let dulces = [];
@@ -9,10 +11,14 @@ let congelados = [];
 
 do {
     let init = prompt("¿Deseas agregar un producto a la lista de compras? \n1) Si \n2) No ");
+    // Flujo de control inicial si-no-invalido
     if (init == "1") {
         let producto = prompt("nombre del producto a ingresar: ");
+        if (producto.trim() == "") {
+            alert("Debe ingresar un nombre válido.");
+            continue;
+        }
         let categoria = prompt("A que categoria pertenece: \n1) frutas  \n2) lacteos  \n3) dulces  \n4) congelados");
-
         switch (categoria) {
             case "1":
                 list(frutas, producto);
@@ -28,7 +34,9 @@ do {
                 break;
             default:
                 alert("Esa categoria no está predefinida.");
+                continue; // Corta la secuencia e inicia de nuevo
         }
+
         alert(`Lista de compras:\n  🍎Frutas: ${frutas}\n  🥛Lácteos: ${lacteos}\n  🍬Dulces: ${dulces}\n  ❄️Congelados: ${congelados}`);
     } else if (init == "2") {
         alert("Saliendo del sistema...");
@@ -36,4 +44,4 @@ do {
     } else {
         alert("Ingresa un numero valido");
     };
-} while (true);
+} while (true); // Bucle infinito hasta que haya un break 
---
+++ day6.js
@@ -0,0 +1,92 @@
+function eliminar(event) {
+    let itemD = event.target.parentElement; // Obtenemos el p donde esta el boton de eliminar
+    let nameP = itemD.querySelector("span").innerText; // Obtiene el nombre del producto
+    let categoria = itemD.querySelectorAll("span")[1].innerText; // Obtiene la categoría
+    // Elimina el elemento del DOM
+    itemD.remove();
+
+    let index;
+
+    // Busca el índice del producto en el array correspondiente según la categoría
+    switch (categoria) {
+        case "Frutas":
+            index = frutas.indexOf(nameP);
+            break;
+        case "Lacteos":
+            index = lacteos.indexOf(nameP);
+            break;
+        case "Dulces":
+            index = dulces.indexOf(nameP);
+            break;
+        case "Congelados":
+            index = congelados.indexOf(nameP);
+            break;
+    }
+
+    console.log(index);
+
+    // También elimina el elemento del array correspondiente
+    switch (categoria) {
+        case "Frutas":
+            frutas.splice(index, 1); // Elimina 1 elemento en la posición `index`
+            console.log(`🍎Frutas: ${frutas}`);
+            break;
+        case "Lacteos":
+            lacteos.splice(index, 1);
+            console.log(`🥛Lácteos: ${lacteos}`);
+            break;
+        case "Dulces":
+            dulces.splice(index, 1);
+            console.log(`🍬Dulces: ${dulces}`);
+            break;
+        case "Congelados":
+            congelados.splice(index, 1);
+            console.log(`❄️Congelados: ${congelados}`);
+            break;
+    }
+}
+
+function crear() {
+    let nameP = val.value.trim();
+    let categoria = select.value;
+
+    if (nameP == "" || categoria == "") {
+        alert("Por favor, ingresa un producto válido.");
+        return;
+    }
+
+    switch (categoria) {
+        case "Frutas": frutas.push(nameP);
+            console.log(`🍎Frutas: ${frutas}`);
+            break;
+        case "Lacteos": lacteos.push(nameP);
+            console.log(` 🥛Lácteos: ${lacteos}`);
+            break;
+        case "Dulces": dulces.push(nameP);
+            console.log(`🍬Dulces: ${dulces}`);
+            break;
+        case "Congelados": congelados.push(nameP);
+            console.log(`❄️Congelados: ${congelados}`);
+            break;
+    }
+
+    let item = document.createElement("p");
+    item.innerHTML = `
+    <span>${nameP}</span>  <span>${categoria}</span>
+    <button onclick="eliminar(event)">Borrar</button>
+`;
+    list.appendChild(item);
+
+    // Resetea los valores al finalizar
+    val.value = "";
+    select.value = "";
+}
+
+let frutas = [];
+let lacteos = [];
+let dulces = [];
+let congelados = [];
+
+let list = document.querySelector(".listContainer");
+let val = document.getElementById("productoInput");
+let select = document.getElementById("opciones");
